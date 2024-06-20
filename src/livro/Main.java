7e2e9764f38196fdae2de7b8411d2f15ec5c5528
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-06-20 09:45:52 -0300

    Innitial Commit

---
+++ src/livro/Main.java
@@ -0,0 +1,12 @@
+package livro;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+	}
+}
